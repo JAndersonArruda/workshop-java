c99a9b953f213eb185f907570cfb65a1a900c43e
JAndersonArruda <ferrazarrudaanderson@gmail.com>
2025-06-17 15:26:38 -0300

    feat: add test profile with H2 database and JPA

--- src/main/java/com/compassoul/workshop/entities/User.java
+++ src/main/java/com/compassoul/workshop/entities/User.java
@@ -1,12 +1,19 @@
 package com.compassoul.workshop.entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serial;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "tb_users")
 public class User implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
