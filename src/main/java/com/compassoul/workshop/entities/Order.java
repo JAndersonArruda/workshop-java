729681e7a8f1aa7058533061aaf5b3fc161cada8
JAndersonArruda <ferrazarrudaanderson@gmail.com>
2025-06-20 13:51:29 -0300

    feat: add Payment entity with one-to-one association

--- src/main/java/com/compassoul/workshop/entities/Order.java
+++ src/main/java/com/compassoul/workshop/entities/Order.java
@@ -30,6 +30,8 @@ public class Order implements Serializable {
     private User client;
     @OneToMany(mappedBy = "id.order")
     private Set<OrderItem> items = new HashSet<>();
+    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+    private Payment payment;
 
     public Order() {
         super();
@@ -76,6 +78,14 @@ public class Order implements Serializable {
 
     public Set<OrderItem> getItems() {
         return items;
+    }
+
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public void setPayment(Payment payment) {
+        this.payment = payment;
     }
 
     @Override
